7d575f765c0e06c4dde56478f5f4054599f829ea
Sarthak Singh <143217268+Sarthak-Singh-24@users.noreply.github.com>
2026-03-29 22:42:45 +0530

    Fix stock detection to always indicate in stock
    Changed the stock detection condition to always return true.

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ while True:
         r = requests.get(URL)
 
         # IMPORTANT FIX: detect ADD TO CART (means IN STOCK)
-        if "ADD TO CART" in r.text:
+        if True:
             print("IN STOCK!!!")
 
             if not in_stock_last_time:
